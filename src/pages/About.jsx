0fd428332cd4a625e921c5c1ec3015ce63b8571b
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-04-20 16:51:47 +0200

    new repository

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         <div className='w-1/2 h-96	'>
           <h3 className=' text-xl px-20 py-6 pt-14 font-medium text-purple-700 drop-shadow-lg	'>A PROPOS DE NOUS</h3>
           <p className=' px-20 text-3xl  py-4 font-semibold drop-shadow-lg	'>La santé est precieuse  et <span className='text-purple-700'>nous voulons en prendre soin</span> </p>
-          <p className='px-20 text-base text-slate-600'> servons notre communauté en leur permettant de poser des questions liées à la santé à ses confrères et consoeurs des différentes spécialités. </p>
+          <p className='px-20 text-base text-slate-600'>Nous servons notre communauté en leur permettant de poser des questions liées à la santé à ses confrères et consoeurs des différentes spécialités. </p>
         </div>
         <div className='w-1/2 h-96 items-center flex justify-start'>
           <Image className=' w-110 drop-shadow-2xl	' src={aboutImg} alt="about"/>
